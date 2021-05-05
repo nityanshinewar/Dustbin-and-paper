3ec5c86e16aadd7c7fa2625c6ed95669e35a8478
nityanshinewar <81683691+nityanshinewar@users.noreply.github.com>
2021-05-05 16:43:31 +0530

    Add files via upload

---
+++ project-c25-template-main/paper.js
@@ -0,0 +1,34 @@
+class Paper{
+	constructor(x,y,r)
+	{
+	// assign options to the rubber ball
+	var options={
+        isStatic:false,
+        restitution:0.3,
+        friction:0,
+        density:1.2
+
+	}
+
+		this.x=x;
+		this.y=y;
+		this.r=r
+		this.body=Bodies.circle(this.x, this.y, (this.r-20)/2, options);
+        this.image=loadImage("paper.png");
+		World.add(world, this.body);
+
+	}
+	display()
+	{
+			var rubberpos=this.body.position;		
+			push()
+			translate(rubberpos.x, rubberpos.y);
+			imageMode(CENTER)
+			strokeWeight(4);
+			stroke("black");
+			fill("darkblue");
+			//draw the ellipse here to display the rubber ball
+            image(this.image,0,0,this.r,this.r)
+			pop()
+	}
+}
